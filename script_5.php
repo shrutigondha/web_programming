39345675fa941c07e7151fc463058ac39f03f736
shrutigondha <155213919+shrutigondha@users.noreply.github.com>
2024-01-16 12:18:25 +0530

    Delete script_5.php

--- script_5.php
+++
@@ -1,9 +0,0 @@
-<html>
-<body>
-<form action="script_5b.php" method="get">
-Firstname:<input type="text" name="firstname"><br><br>
-Lastname:<input type="text" name="lastname"><br><br>
-<input type="submit" name="submit">
-</form>
-</body>
-</html>
