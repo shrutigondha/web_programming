a012086285be7a40ccd1506e46e61cf3ae2e36cd
shrutigondha <155213919+shrutigondha@users.noreply.github.com>
2024-01-17 11:02:23 +0530

    Delete script15_b.php

--- script15_b.php
+++
@@ -1,8 +0,0 @@
-<html>
-<form method="get" action="script15.php">
-Enter val1:<input type="text" name="val_1"><br>
-Enter val2:<input type="text" name="val_2"><br>
-Enter val3:<input type="text" name="val_3"><br>
-<input type="submit" name="submit">
-</form>
-</html>
